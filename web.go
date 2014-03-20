595a6181d52bec416e667878e62dee3c5264cd2c
Diwank Singh <diwank.singh@gmail.com>
2014-03-19 17:53:23 -0700

    Switched to martini <clink glasses>
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- web.go
+++ web.go
@@ -1,35 +1,25 @@
 package main
 
 import (
-	"html/template"
+	martini "github.com/codegangsta/martini"
 	"net/http"
-	"os"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func hello(w http.ResponseWriter, req *http.Request) string {
 	// Set HTTP headers.
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	// Parse template.
-	t := template.Must(template.New("index").Parse(`
-  <html>
+	return `
     <title> Toy Server </title>
     <body>
       Lady Gagagaagagaga...
-    </body>
-  </html>
-  `))
-
-	if e := t.Execute(w, nil); e != nil {
-		panic("template render issue")
-	}
+  `
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.Handle("/static", http.FileServer(http.Dir("./static")))
+	m := martini.Classic()
+	m.Get("/", hello)
 
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		panic(err)
-	}
+	m.Run()
 }
